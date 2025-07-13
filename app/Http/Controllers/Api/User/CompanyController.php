f61d266a3442a999dc47488303e654725ce90d01
Marwan Gaber <fk413691@gmail.com>
2025-07-13 12:47:43 +0300

    search company

--- app/Http/Controllers/Api/User/CompanyController.php
+++ app/Http/Controllers/Api/User/CompanyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\User;
 use App\Http\Controllers\Controller;
 use App\Models\Company;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CompanyController extends Controller
 {
@@ -14,6 +15,35 @@ class CompanyController extends Controller
     {
         $companies = Company::whereIn('status', ['approved', 'active'])
             ->with(['companySpecializations.specialization','drugs'])
+            ->get();
+
+        return response()->json([
+            'companies' => $companies
+        ]);
+    }
+
+    public function searchCompanies(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'specialization_id' => 'nullable|exists:specializations,id',
+            'country_id' => 'nullable|exists:countries,id',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()->first()], 422);
+        }
+
+        $companies = Company::whereIn('status', ['approved', 'active'])
+            ->when($request->country_id, function ($query) use ($request) {
+                $query->where('country_id', $request->country_id);
+            })
+            ->when($request->specialization_id, function ($query) use ($request) {
+                $query->whereHas('companySpecializations', function ($q) use ($request) {
+                    $q->where('specialization_id', $request->specialization_id)
+                    ->where('status', 'active'); // optional filter
+                });
+            })
+            ->with(['companySpecializations.specialization', 'country:id,name', 'city:id,name'])
             ->get();
 
         return response()->json([
